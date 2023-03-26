df1feaf0d4e66343f120c2e85f2b2ff91a8961f6
Fırat Alçın <firatalcin04@gmail.com>
2023-03-26 03:22:13 +0300

    Generic Repository

--- PortfolioProject/PortfolioProject.DataAccess/Repository/GenericRepository.cs
+++ PortfolioProject/PortfolioProject.DataAccess/Repository/GenericRepository.cs
@@ -1,11 +1,6 @@
 ﻿using PortfolioProject.DataAccess.Abstract;
 using PortfolioProject.DataAccess.Concrete.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PortfolioProject.DataAccess.Repository
 {
