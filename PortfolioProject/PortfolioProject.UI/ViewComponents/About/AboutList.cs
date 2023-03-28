726e1f9bf5fce557c27064fa35b8da25ff22246f
Fırat Alçın <firatalcin04@gmail.com>
2023-03-29 02:51:54 +0300

    About List

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/About/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.About
+{
+    public class AboutList : ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = aboutManager.GetAll();
+            return View(values);
+        }
+    }
+}
