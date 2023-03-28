de56063385dbbe4c9ef8bb6dbf7caf0a025a546b
Fırat Alçın <firatalcin04@gmail.com>
2023-03-29 01:30:35 +0300

    Feature List

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/FeatureManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class FeatureManager : IGenericService<Feature>
+    {
+        IFeatureDal featureDal;
+
+        public FeatureManager(IFeatureDal featureDal)
+        {
+            this.featureDal = featureDal;
+        }
+
+        public void Add(Feature t)
+        {
+            featureDal.Add(t);
+        }
+
+        public void Delete(Feature t)
+        {
+            featureDal.Delete(t);   
+        }
+
+        public List<Feature> GetAll()
+        {
+            return featureDal.GetAll();
+        }
+
+        public Feature GetByID(int id)
+        {
+            return featureDal.Get(x => x.Id == id);
+        }
+
+        public void Update(Feature t)
+        {
+            featureDal.Update(t);
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Feature
+{
+    public class FeatureList : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.GetAll();
+            return View(values);
+        }
+    }
+}
