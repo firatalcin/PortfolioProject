2bf5abd89b79ea6465fb93ad344e9a635ee3ac73
Fırat Alçın <firatalcin04@gmail.com>
2023-03-29 03:01:44 +0300

    Service List

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class ServiceManager : IGenericService<Service>
+    {
+        IServiceDal serviceDal;
+
+        public ServiceManager(IServiceDal serviceDal)
+        {
+            this.serviceDal = serviceDal;
+        }
+
+        public void Add(Service t)
+        {
+            serviceDal.Add(t);
+        }
+
+        public void Delete(Service t)
+        {
+            serviceDal.Delete(t);
+        }
+
+        public List<Service> GetAll()
+        {
+            return serviceDal.GetAll();
+        }
+
+        public Service GetByID(int id)
+        {
+            return serviceDal.Get(x => x.Id == id);
+        }
+
+        public void Update(Service t)
+        {
+            serviceDal.Update(t);
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Service
+{
+    public class ServiceList : ViewComponent
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = serviceManager.GetAll();
+            return View(values);
+        }
+    }
+}
