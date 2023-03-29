0e25db6240da320e8f1bda572c87f01a905e5df3
Fırat Alçın <firatalcin04@gmail.com>
2023-03-29 19:32:06 +0300

    Skill List

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/SkillManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class SkillManager : IGenericService<Skill>
+    {
+        ISkillDal _skillDal;
+
+        public SkillManager(ISkillDal skillDal)
+        {
+            _skillDal = skillDal;
+        }
+
+        public void Add(Skill t)
+        {
+            _skillDal.Add(t);
+        }
+
+        public void Delete(Skill t)
+        {
+            _skillDal.Delete(t);    
+        }
+
+        public List<Skill> GetAll()
+        {
+           return  _skillDal.GetAll();
+        }
+
+        public Skill GetByID(int id)
+        {
+            return _skillDal.Get(x => x.ID == id);
+        }
+
+        public void Update(Skill t)
+        {
+            _skillDal.Update(t);
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = skillManager.GetAll();
+            return View(values);
+        }
+    }
+}
