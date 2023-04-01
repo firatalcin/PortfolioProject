0f981cb3fbcf2bf573c81ae79c04cd817675a4c9
Fırat Alçın <firatalcin04@gmail.com>
2023-04-01 19:17:45 +0300

    Testimonial List

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/TestimonialManager.cs
@@ -0,0 +1,41 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class TestimonialManager : IGenericService<Testimonial>
+    {
+        ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void Add(Testimonial t)
+        {
+            _testimonialDal.Add(t);
+        }
+
+        public void Delete(Testimonial t)
+        {
+            _testimonialDal.Delete(t);
+        }
+
+        public List<Testimonial> GetAll()
+        {
+            return _testimonialDal.GetAll();
+        }
+
+        public Testimonial GetByID(int id)
+        {
+            return _testimonialDal.Get(x => x.ID.Equals(id));
+        }
+
+        public void Update(Testimonial t)
+        {
+            _testimonialDal.Update(t);
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Testimonial/TestimonialList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Testimonial
+{
+    public class TestimonialList : ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var testimonials = testimonialManager.GetAll();
+            return View(testimonials);
+        }
+    }
+}
