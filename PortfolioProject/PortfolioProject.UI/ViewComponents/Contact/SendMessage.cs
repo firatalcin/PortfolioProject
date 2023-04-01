84cbff1567f9a176a2f35b1d2b36ca213fa80c39
Fırat Alçın <firatalcin04@gmail.com>
2023-04-01 19:27:53 +0300

    İletişim Alanları

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.UI.ViewComponents.Contact
+{
+    public class SendMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
