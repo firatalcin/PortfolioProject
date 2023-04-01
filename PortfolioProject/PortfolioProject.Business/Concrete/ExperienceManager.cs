83e033ee302777751e8eb56bb393cf8f95238890
Fırat Alçın <firatalcin04@gmail.com>
2023-04-01 15:34:54 +0300

    Experience List 1

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/ExperienceManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class ExperienceManager : IGenericService<Experience>
+    {
+        IExperienceDal _experienceDal;
+
+        public ExperienceManager(IExperienceDal experienceDal)
+        {
+            _experienceDal = experienceDal;
+        }
+
+        public void Add(Experience t)
+        {
+            _experienceDal.Add(t);
+        }
+
+        public void Delete(Experience t)
+        {
+            _experienceDal.Delete(t);
+        }
+
+        public List<Experience> GetAll()
+        {
+            return _experienceDal.GetAll();
+        }
+
+        public Experience GetByID(int id)
+        {
+            return _experienceDal.Get(x => x.ID == id);
+        }
+
+        public void Update(Experience t)
+        {
+            _experienceDal.Update(t);   
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Experience
+{
+    public class ExperienceList : ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.GetAll();
+            return View(values);
+        }
+    }
+}
