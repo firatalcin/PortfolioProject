f38f1032e5f217779d199181f512e52bb2d8e744
Fırat Alçın <firatalcin04@gmail.com>
2023-04-01 15:16:34 +0300

    Portfolio List

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/PortfolioManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class PortfolioManager : IGenericService<Portfolio>
+    {
+        IPortfolioDal _portfolioDal;
+
+        public PortfolioManager(IPortfolioDal portfolioDal)
+        {
+            _portfolioDal = portfolioDal;
+        }
+
+        public void Add(Portfolio t)
+        {
+            _portfolioDal.Add(t);
+        }
+
+        public void Delete(Portfolio t)
+        {
+            _portfolioDal.Delete(t);
+        }
+
+        public List<Portfolio> GetAll()
+        {
+            return _portfolioDal.GetAll();  
+        }
+
+        public Portfolio GetByID(int id)
+        {
+            return _portfolioDal.Get(x => x.ID == id);
+        }
+
+        public void Update(Portfolio t)
+        {
+            _portfolioDal.Update(t);
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var portfolios = portfolioManager.GetAll();
+            return View(portfolios);
+        }
+    }
+}
