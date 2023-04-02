b42060b2581337ddf35426ac08a61164479ac501
Fırat Alçın <firatalcin04@gmail.com>
2023-04-02 23:11:39 +0300

    Mesaj Paneli Hazırlığı

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class MessageManager : IGenericService<Message>
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void Add(Message t)
+        {
+            _messageDal.Add(t);
+        }
+
+        public void Delete(Message t)
+        {
+            _messageDal.Delete(t);
+        }
+
+        public List<Message> GetAll()
+        {
+            return _messageDal.GetAll();
+        }
+
+        public Message GetByID(int id)
+        {
+            return _messageDal.Get(x => x.ID == id);
+        }
+
+        public void Update(Message t)
+        {
+            _messageDal.Update(t);
+        }
+    }
+}
--- PortfolioProject/PortfolioProject.UI/ViewComponents/Contact/SendMessage.cs
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Contact/SendMessage.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
 
 namespace PortfolioProject.UI.ViewComponents.Contact
 {
     public class SendMessage : ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+        [HttpGet]
         public IViewComponentResult Invoke()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IViewComponentResult Invoke(Message message)
+        {
+            message.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message.Status = true;
+            messageManager.Add(message); 
             return View();
         }
     }
