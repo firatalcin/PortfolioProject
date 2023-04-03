082088efc8402f2eeb6afafbc22fa13241fd02a7
Fırat Alçın <firatalcin04@gmail.com>
2023-04-03 23:16:21 +0300

    Layout Hazırlığı

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/FeatureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
