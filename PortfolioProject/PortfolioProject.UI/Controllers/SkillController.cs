7a2f2b80e6c806f2c94c8913212d5a3b69ff4c33
Fırat Alçın <firatalcin04@gmail.com>
2023-04-04 22:32:07 +0300

    Yeteneklerin Listelenmesi

--- PortfolioProject/PortfolioProject.UI/Controllers/SkillController.cs
+++ PortfolioProject/PortfolioProject.UI/Controllers/SkillController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
 
 namespace PortfolioProject.UI.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            var values = skillManager.GetAll();
             return View();
         }
     }
