21a41c2e08462b27f96ef858ddd2618c455da36c
Fırat Alçın <firatalcin04@gmail.com>
2023-04-07 01:32:50 +0300

    Deneyim Güncelleme ve Silme

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/ExperienceController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IActionResult Index()
+        {
+            var values = experienceManager.GetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddExperience() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience) 
+        {
+            experienceManager.Add(experience);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditExperience(int id) 
+        {
+            var entity = experienceManager.GetByID(id);
+            return View(entity);   
+        }
+
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience)
+        {
+            experienceManager.Update(experience);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var entity = experienceManager.GetByID(id);
+            experienceManager.Delete(entity);
+            return RedirectToAction("Index");
+        }
+    }
+}
