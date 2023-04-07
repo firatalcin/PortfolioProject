a9ec852eaa8df97b232c11f0dac349848213b849
Fırat Alçın <firatalcin04@gmail.com>
2023-04-08 01:00:23 +0300

    Portfolio Listelenmesi

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/PortfolioController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IActionResult Index()
+        {
+            var values = portfolioManager.GetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddPortfolio() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            portfolioManager.Add(portfolio);
+            return RedirectToAction("Index");
+        }
+    }
+}
