23dc71274e4508ee41177b0c62736961a55a19e3
Fırat Alçın <firatalcin04@gmail.com>
2023-04-08 14:44:39 +0300

    Service List

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/ServiceController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+
+        public IActionResult Index()
+        {
+            var values = serviceManager.GetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.Add(service);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.GetByID(id);
+            serviceManager.Delete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            var values = serviceManager.GetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            serviceManager.Update(service);
+            return RedirectToAction("Index");
+        }
+    }
+}
