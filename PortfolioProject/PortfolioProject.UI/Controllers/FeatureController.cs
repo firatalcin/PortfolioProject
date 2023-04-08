72a6aae4ad0ced09ed5441b195efc3d263c35240
Fırat Alçın <firatalcin04@gmail.com>
2023-04-08 13:05:24 +0300

    Feature Düzenlemeleri

--- PortfolioProject/PortfolioProject.UI/Controllers/FeatureController.cs
+++ PortfolioProject/PortfolioProject.UI/Controllers/FeatureController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
 
 namespace PortfolioProject.UI.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var values = featureManager.GetByID(1);
+            return View(values);
         }
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.Update(feature);
+            return RedirectToAction("Index", "Default");
+        }
+
+
     }
 }
