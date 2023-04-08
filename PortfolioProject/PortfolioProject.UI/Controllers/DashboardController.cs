9a2b8906b6ed64fb31cc7527db1fc55c6225c19c
Fırat Alçın <firatalcin04@gmail.com>
2023-04-08 14:56:04 +0300

    Dashboard

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
