52b1bc321392248f475b4476b8ddeec241dc0539
Fırat Alçın <firatalcin04@gmail.com>
2023-04-08 13:09:28 +0300

    About Düzenlemeleri

---
+++ PortfolioProject/PortfolioProject.UI/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+using PortfolioProject.Entity.Concrete;
+
+namespace PortfolioProject.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = aboutManager.GetByID(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            aboutManager.Update(about);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
