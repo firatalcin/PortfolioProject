50ca564eb762a16a7771808b5e97f48eee970bc1
Fırat Alçın <firatalcin04@gmail.com>
2023-04-10 23:53:58 +0300

    Dashboard Feature Statistics

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.DataAccess.Concrete.Context;
+
+namespace PortfolioProject.UI.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+        ApplicationContext context = new ApplicationContext();
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Where(x => x.Status == false).Count();
+            ViewBag.v3 = context.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = context.Experiences.Count();
+            return View();
+        }
+    }
+}
