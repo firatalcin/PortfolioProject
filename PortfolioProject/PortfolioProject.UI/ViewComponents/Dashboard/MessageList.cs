eb44cae9d7bc662c32e544af1966610024ec7af6
Fırat Alçın <firatalcin04@gmail.com>
2023-04-11 19:07:22 +0300

    Dashboard Mesaj Paneli

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Dashboard
+{
+    public class MessageList : ViewComponent
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = messageManager.GetAll().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
