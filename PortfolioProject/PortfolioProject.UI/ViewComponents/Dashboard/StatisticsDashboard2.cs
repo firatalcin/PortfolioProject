54f1534633636c486af987447325ea2335516e4b
Fırat Alçın <firatalcin04@gmail.com>
2023-04-11 19:01:05 +0300

    Dashboard İstatistik 2

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.DataAccess.Concrete.Context;
+
+namespace PortfolioProject.UI.ViewComponents.Dashboard
+{
+    public class StatisticsDashboard2 : ViewComponent
+    {
+        ApplicationContext c = new ApplicationContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Portfolios.Count();
+            ViewBag.v2 = c.Messages.Count();
+            ViewBag.v3 = c.Services.Count();
+            return View();
+        }
+    }
+}
