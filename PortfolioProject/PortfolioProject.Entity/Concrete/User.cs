c57d0b02cab0c6a0850234ad6c1d46d4f01ea3cd
Fırat Alçın <firatalcin04@gmail.com>
2023-04-11 19:11:35 +0300

    User Tablosu

---
+++ PortfolioProject/PortfolioProject.Entity/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Entity.Concrete
+{
+    public class User
+    {
+        public int Id { get; set; }   
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string ImageURL { get; set; }
+        public bool Status { get; set; }
+    }
+}
