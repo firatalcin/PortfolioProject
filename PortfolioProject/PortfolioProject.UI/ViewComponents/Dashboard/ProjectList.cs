2dbe49a705a8e4c6656a3d2bdfd0bc0b10afee00
Fırat Alçın <firatalcin04@gmail.com>
2023-04-11 19:03:58 +0300

    Dashboard Proje Listesi

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Dashboard/ProjectList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Dashboard
+{
+    public class ProjectList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.GetAll();
+            return View(values);
+        }
+    }
+}
