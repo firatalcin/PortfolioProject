dce1d688908c82ce77c1bb8cd176cfe3e4f53f51
Fırat Alçın <firatalcin04@gmail.com>
2023-04-12 23:40:25 +0300

    Slide Paneli

---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Portfolio
+{
+    public class SlideList : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.GetAll();
+            return View(values);
+        }
+    }
+}
