83759df9a63503745cb2bb4a04c89f851420aff6
Fırat Alçın <firatalcin04@gmail.com>
2023-04-15 01:55:42 +0300

    ToDoList Listesi

---
+++ PortfolioProject/PortfolioProject.Business/Concrete/ToDoListManager.cs
@@ -0,0 +1,45 @@
+﻿using PortfolioProject.Business.Abstract;
+using PortfolioProject.DataAccess.Abstract;
+using PortfolioProject.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioProject.Business.Concrete
+{
+    public class ToDoListManager : IToDoListService
+    {
+        IToDoListDal _toDoListDal;
+
+        public ToDoListManager(IToDoListDal toDoListDal)
+        {
+            _toDoListDal = toDoListDal;
+        }
+        public void Add(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoList> GetAll()
+        {
+            return _toDoListDal.GetAll();
+        }
+
+        public ToDoList GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ToDoList t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PortfolioProject/PortfolioProject.UI/ViewComponents/Dashboard/ToDoListPanel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject.Business.Concrete;
+using PortfolioProject.DataAccess.Concrete.EntityFramework;
+
+namespace PortfolioProject.UI.ViewComponents.Dashboard
+{
+    public class ToDoListPanel : ViewComponent
+    {
+        ToDoListManager toDoListManager = new ToDoListManager(new EfToDoListDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = toDoListManager.GetAll();
+            return View(values);
+        }
+    }
+}
